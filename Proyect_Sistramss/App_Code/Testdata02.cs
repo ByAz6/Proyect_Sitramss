6ae09125149efc002158e45f1b41a94eabadd157
ByAz <brianzepeda239@hotmail.com>
2020-12-01 00:34:23 -0600

    AppData02

---
+++ Proyect_Sistramss/App_Code/Testdata02.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Testdata02
+/// </summary>
+public class Testdata02
+{
+    public Testdata02()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
