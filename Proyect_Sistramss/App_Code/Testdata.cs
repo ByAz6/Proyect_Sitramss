fe86beed0e0e8dd6b54039718e5ccd90d99dfbd9
Pc Usuario <Pc Usuario@BRYANELAZ>
2020-11-30 23:41:49 -0600

    Agregacion de app_code

---
+++ Proyect_Sistramss/App_Code/Testdata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Testdata
+/// </summary>
+public class Testdata
+{
+    public Testdata()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
